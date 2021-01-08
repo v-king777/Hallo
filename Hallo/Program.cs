642a29ddf924f754a1a19a5f431264a6c5b15648
v-kolp <v.kolpashchikov@gmail.com>
2021-01-08 19:53:34 +0300

    Добавьте файлы проекта.

---
+++ Hallo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hallo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите своё имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, {0}!", name);
+            Console.WriteLine("Нажмите <Enter> для выхода ...");
+            Console.Read();
+        }
+    }
+}
