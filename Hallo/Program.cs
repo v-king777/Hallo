8e3c1cafaa5b0adf50f59b6e8a9c790cf4ad9987
v-kolp <v.kolpashchikov@gmail.com>
2021-01-08 23:46:05 +0300

    modified:   Hallo/Program.cs

--- Hallo/Program.cs
+++ Hallo/Program.cs
@@ -6,9 +6,13 @@ namespace Hallo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите своё имя:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Привет, {0}!", name);
+            string MyName = "ЭВМ";
+            Console.WriteLine("Привет, человек!");
+            Console.WriteLine("Меня зовут {0}", MyName);
+            Console.WriteLine("А как тебя зовут?");
+            string YourName = Console.ReadLine();
+            Console.WriteLine("Рад знакомству, {0}!", YourName);
+
             Console.WriteLine("\nДля продолжения нажмите любую клавишу . . .");
             Console.ReadKey();
         }
